d381fcd0a715d13ad93bd408171a2fcc20d33ddd
Eduardo-Alanis-Garcia <al440184@uaeh.edu.mx>
2025-07-14 23:19:57 -0600

    Se añadieron mas bases

--- Ver_Base_Datos.R
+++ Ver_Base_Datos.R
@@ -1,1 +1,10 @@
 datos = read.csv("Datos/netflix_titles.csv")
+conteo_tipo = datos |> 
+  dplyr::group_by(type) |> 
+  dplyr::summarise(conteo = dplyr::n())
+
+conteo_director = datos |> 
+  dplyr::group_by(director) |> 
+  dplyr::summarise(conteo = dplyr::n())
+
+
